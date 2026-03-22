8400ee2bda7074d4b726c9a0ffa8fede2ae0ae67
grobar93 <rgdQcykDcFRJnF4wazpp>
2026-03-22 03:57:56 +0100

    fix: update 404 page to use localePath for router-link navigation

--- pages/404.vue
+++ pages/404.vue
@@ -6,13 +6,17 @@
     <ul>
       <li>{{ $t('layout.notFound.item1') }}</li>
       <li>{{ $t('layout.notFound.item2') }}
-        <router-link class="text-primary" :to="{name: 'index'}">{{$t('layout.notFound.here')}}</router-link>
+        <router-link class="text-primary" :to="localePath('/')">{{$t('layout.notFound.here')}}</router-link>
         {{ $t('layout.notFound.item3') }}
       </li>
       <li>{{ $t('layout.notFound.item4') }}</li>
     </ul>
   </v-container>
 </template>
+
+<script setup>
+const localePath = useLocalePath()
+</script>
 
 <style scope>
 ul{
